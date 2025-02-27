a75cd22643229613c2f48d72fb83f7ea46d79a6c
Benjamin de Chazeaux <dechazeauxb@gmail.com>
2025-02-27 12:17:53 +0100

    Rails watch list ajout d'images

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to @bookmark, notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_url, notice: 'Bookmark was successfully destroyed.'
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+puts "Creating movies..."
+
+# Remplacez ACCESS_TOKEN par votre véritable token d'API TMDb
+# My API key: b882a0d8be700b365b64176607645a3
+# My API read access token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTgyYTBkODhiZTcwMGIzNjViNjQxNzY2MDc2NDVhMyIsIm5iZiI6MTc0MDY1MTk3Ny40NjQsInN1YiI6IjY3YzAzZGM5ODM0MDU4ZjE2YWM4ZDdhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SRkoYRfrRHWbJLyIIS-JyzxPi61p-xOkl4oxTXxMLyI
+
+url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200"
+authorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTgyYTBkODhiZTcwMGIzNjViNjQxNzY2MDc2NDVhMyIsIm5iZiI6MTc0MDY1MTk3Ny40NjQsInN1YiI6IjY3YzAzZGM5ODM0MDU4ZjE2YWM4ZDdhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SRkoYRfrRHWbJLyIIS-JyzxPi61p-xOkl4oxTXxMLyI"
+
+movies_serialized = URI.open(url, "Authorization" => authorization).read
+movies = JSON.parse(movies_serialized)
+
+movies["results"].each do |movie_data|
+  Movie.create!(
+    title: movie_data["title"],
+    overview: movie_data["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie_data["poster_path"]}",
+    rating: movie_data["vote_average"]
+  )
+  puts "Created #{movie_data["title"]}"
+end
+
+puts "Creating lists..."
+["Action", "Comedy", "Drama", "Classic", "Horror"].each do |name|
+  list = List.create!(name: name)
+  puts "Created #{name} list"
+end
+
+puts "Finished!"
